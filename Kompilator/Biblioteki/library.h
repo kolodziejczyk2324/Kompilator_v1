06369397d7c46dca3363d9766e27aa8f923fc6f5
Przemek Kołodziejczyk <kemeros13@wp.pl>
2016-12-21 18:51:57 +0100

    Pierwsza kompletna wersja kompilatora

---
+++ Kompilator/Biblioteki/library.h
@@ -0,0 +1,16 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+void create_number(vector<string> &code, unsigned long long int a, int r);
+
+string concatStringInt(string s, int i);
+
+long long int numbSub(unsigned long long int a, unsigned long long int b);
+
+int isPrefix(string pattern, string text);
+
+string getLastWord(string tekst);
+
+string replaceLastWord(string tekst, string rep);
